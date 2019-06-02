9704d19df9657b98e7c1fb7d14d5d7f3bbbcd985
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-01 21:55:22 -0300

    desabilita o logo do google

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/O que o JavaScript é capaz de fazer? - Curso JavaScript #01/console.dev.tools.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/O que o JavaScript é capaz de fazer? - Curso JavaScript #01/console.dev.tools.js
@@ -3,3 +3,5 @@ alert('Olá mundo!')
 document.body.style.background='#000'
 
 document.querySelector('img#hplogo').style.filter = 'grayScale(100%)'
+
+document.querySelector('img#hplogo').style.visibility = 'hidden'
