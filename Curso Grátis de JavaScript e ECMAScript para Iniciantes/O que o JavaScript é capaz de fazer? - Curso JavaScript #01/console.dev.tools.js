20026937eff3d6bff12daf01e562ad76bfec9165
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-02 05:27:22 -0300

    exemplo de troca de  texto com innerText

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/O que o JavaScript é capaz de fazer? - Curso JavaScript #01/console.dev.tools.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/O que o JavaScript é capaz de fazer? - Curso JavaScript #01/console.dev.tools.js
@@ -9,3 +9,5 @@ document.querySelector('img#hplogo').style.visibility = 'hidden'
 
 document.querySelector('span.bstn-hl-title').textContent = 'Raquel Comemora aniversário da Bibi junto a amigos e parentes'
 document.querySelector('span.bstn-related').textContent = 'Bibi diz que ficou muito feliz com evento'
+document.querySelector('span.bstn-hl-title').innerText = 'Raquel Comemora aniversário da Bibi junto a amigos e parentes'
+document.querySelector('span.bstn-related').innerText = 'Bibi diz que ficou muito feliz com evento'
