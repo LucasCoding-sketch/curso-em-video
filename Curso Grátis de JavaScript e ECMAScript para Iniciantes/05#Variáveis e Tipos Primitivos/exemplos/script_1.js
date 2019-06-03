2e386d26c10f938018c1650365d37df23580f3a2
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-03 16:52:38 -0300

    cria exemplos de uso

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/05#Variáveis e Tipos Primitivos/exemplos/script_1.js
@@ -0,0 +1,9 @@
+var nome = "Foo"
+let sobrenome = 'Boo'
+var nomeCompleto = `Foo Boo`
+let média = (3+6)/2
+
+console.log(nome)
+console.log(sobrenome)
+console.log(nomeCompleto)
+console.log(média)
