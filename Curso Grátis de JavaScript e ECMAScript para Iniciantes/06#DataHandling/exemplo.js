ad97e10476e15ffda827340c9492f3a3fa0a759b
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-04 14:59:53 -0300

    toLocaleString

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exemplo.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exemplo.js
@@ -19,3 +19,16 @@ console.log(decimal_number.toFixed(0))
 console.log(decimal_number.toFixed(1))
 console.log(decimal_number.toFixed(2))
 console.log(decimal_number.toFixed(4))
+
+
+var salario = 1200.00
+
+var locale = 'pt-BR'
+
+var oprtionsObjectToMyLocaleString = {style: 'currency', 'currency': 'BRL' }
+
+salario = salario.toLocaleString(locale, oprtionsObjectToMyLocaleString)
+
+
+console.log(salario)
+//R$1,200.00
