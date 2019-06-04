4b138f7dd2376b8f232de02c6b5ac1e18188d58f
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-04 12:14:35 -0300

    Nova sintaxy

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exercicio#03/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exercicio#03/script.js
@@ -1,7 +1,7 @@
 var um_numero = window.prompt('Informe um número:')
-var outro_numero = Number.parseFloat(window.prompt('Informe outro número'))
+var outro_numero = Number(window.prompt('Informe outro número'))
 
-um_numero = Number.parseInt(um_numero)
+um_numero = Number(um_numero)
 var resultado = um_numero + outro_numero
 
 window.alert("Resposta " + resultado)
