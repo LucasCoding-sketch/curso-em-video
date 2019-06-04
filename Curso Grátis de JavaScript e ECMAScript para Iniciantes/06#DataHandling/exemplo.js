37006f91ba23a205a00e31e49c58fd2bbd505161
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-04 12:28:55 -0300

    sem o template string

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exemplo.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exemplo.js
@@ -1,3 +1,11 @@
 var linguagem = 'Java Script'
 
 console.log("Eu estou aprendendo " + linguagem)
+
+
+nome = 'Boo'
+idade = 42
+nota = 4.6
+
+console.log('O aluno ' + nome + " com a idade de: " + idade + ' tirou nota ' + nota)
+//'O aluno Boo com a idade de: 42 tirou nota 4.6'
