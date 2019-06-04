e1caafd856d62219cb28ca93473497e01e1959a1
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-04 18:57:56 -0300

    precedencia

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo#A/07#Operadores/exemplos.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo#A/07#Operadores/exemplos.js
@@ -3,3 +3,13 @@ divisao = 9/2
 precedencia_de_operadores = 5+3/2
 precedencia_de_operadores_meida = (5+3)/2
 potencia = 5**2
+
+
+// precedencia
+var a = 5 + 3 //8
+var b = a % 5 //3
+var c = 5 * b ** 2 //45
+var d = 10 - a / 2 //6
+var e = 6 * 2 / d // 2
+var f = b % e + 4 / e //3
+console.log(f)
