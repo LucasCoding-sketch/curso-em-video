89cec183d491b04ea14f9e4296f48db30fc8dcb1
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-04 12:06:03 -0300

    concatena duas strings

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exercicio#03/script.js
@@ -0,0 +1,5 @@
+var um_numero = window.prompt('Informe um número:')
+var outro_numero = window.prompt('Informe outro número')
+var resultado = um_numero + outro_numero
+
+window.alert("Resposta " + resultado)
