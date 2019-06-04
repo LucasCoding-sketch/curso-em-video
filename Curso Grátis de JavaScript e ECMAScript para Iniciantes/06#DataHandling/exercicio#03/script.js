b84aa38774e7003e8d6b1703d85fd0861282372f
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-04 12:09:50 -0300

    Use Number.parse...

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exercicio#03/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/06#DataHandling/exercicio#03/script.js
@@ -1,5 +1,7 @@
 var um_numero = window.prompt('Informe um número:')
-var outro_numero = window.prompt('Informe outro número')
+var outro_numero = Number.parseFloat(window.prompt('Informe outro número'))
+
+um_numero = Number.parseInt(um_numero)
 var resultado = um_numero + outro_numero
 
 window.alert("Resposta " + resultado)
