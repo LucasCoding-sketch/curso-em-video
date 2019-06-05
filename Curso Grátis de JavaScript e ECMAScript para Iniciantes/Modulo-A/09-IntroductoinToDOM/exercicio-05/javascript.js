32f1db2ae9bb7551b3b117859483309e0f817178
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-05 16:05:47 -0300

    getElementsByTagName

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-A/09-IntroductoinToDOM/exercicio-05/javascript.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-A/09-IntroductoinToDOM/exercicio-05/javascript.js
@@ -3,3 +3,12 @@ console.log('ola mundo')
 console.log(window.navigator.appName)
 console.log(window.navigator.appCodeName)
 console.log(window.document.URL)
+
+var firstParagraph = window.document.getElementsByTagName('p')[0]
+console.log(firstParagraph)
+window.document.write(firstParagraph.innerText)
+firstParagraph.style.color = "#333"
+firstParagraph.style.backgroundColor = "#999"
+
+var secoundParagraph = window.document.getElementsByTagName('p')[1]
+window.document.write("<br>" + secoundParagraph.innerHTML)
