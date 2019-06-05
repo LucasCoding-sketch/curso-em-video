870d04f6bc0f7ca3b5c8dc877b96535ca887606d
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-05 05:32:18 -0300

    iniciando operadores relacionais

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo#A/08#Operadores/exemplos.js
@@ -0,0 +1,11 @@
+//Operadores relacionais
+console.log("\/\/Operadores relacionais\n")
+
+console.log(5 >  2)
+console.log(7 <  7)
+console.log(8 >= 8)
+console.log(9 <= 7)
+console.log(5 == 5)
+console.log(4 != 4)
+
+console.log('--------------------\n')
