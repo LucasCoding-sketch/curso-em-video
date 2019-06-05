a8b95a75005464f49ca5d1335496aaabf084cfb1
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-05 17:40:55 -0300

    adiciona metodo de soma

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-A/10-DOMEvents/exercicio-07/script.js
@@ -0,0 +1,17 @@
+function somar(){
+    var tn1 = document.querySelector("input#txtn1")
+
+    var tn2 = document.querySelector("input#txtn2")
+
+    var resultado = document.querySelector("div#resultado")
+
+    var n1 = Number(tn1.value)
+
+    var n2 = Number(tn2.value)
+
+    var s = n1 + n2
+
+    resultado.innerHTML = `A Soma entre ${n1} + ${n2} é : ${s}`
+}
+
+document.querySelector("#somar").addEventListener('click', somar)
