c07ef6294ee34c0058a3c3754683ffd22ff48174
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-05 16:10:34 -0300

    getElementById

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-A/09-IntroductoinToDOM/exercicio-05/javascript.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-A/09-IntroductoinToDOM/exercicio-05/javascript.js
@@ -12,3 +12,6 @@ firstParagraph.style.backgroundColor = "#999"
 
 var secoundParagraph = window.document.getElementsByTagName('p')[1]
 window.document.write("<br>" + secoundParagraph.innerHTML)
+
+var div = window.document.getElementById('msg')
+div.style.backgroundColor = 'green'
