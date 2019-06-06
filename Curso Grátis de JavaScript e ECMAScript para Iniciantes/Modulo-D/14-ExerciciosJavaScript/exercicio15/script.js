2a237d73850ddef40f8942c8f51c760f4ef7beae
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-06 14:55:05 -0300

    adiona as classes de core para cada momento do dia

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/14-ExerciciosJavaScript/exercicio15/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/14-ExerciciosJavaScript/exercicio15/script.js
@@ -1,19 +1,26 @@
-console.log('exercicio x')
 function loader(){
     var message = document.getElementById('message')
+
     var image = document.querySelector('#show-image')
+
     var date = new Date()
 
     var hour = date.getHours()
-    console.log(image.src)
-    message.innerHTML = `Agora são: ${hour} horas`
 
+    message.innerHTML = `Agora são: ${hour} horas`
+    
     if (hour >=0 && hour < 12){
         image.src = 'img/foto-manha.png'
+        document.body.classList.remove('tarde', 'noite')
+        document.body.classList.add('manha')
     }else if (hour >= 12 && hour < 18){
         image.src = 'img/foto-tarde.png'
+        document.body.classList.remove('manha', 'noite')
+        document.body.classList.add('tarde')
     }else{
         image.src = 'img/foto-noite.png'
+        document.body.classList.remove('manha', 'tarde')
+        document.body.classList.add('noite')
     }
 }
 
