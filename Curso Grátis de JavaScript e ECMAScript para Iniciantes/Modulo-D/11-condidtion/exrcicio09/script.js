be5f716c7a49fc53a8b13a21305af88874ad54b3
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-06 06:04:58 -0300

    condicional composta

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/11-condidtion/exrcicio09/script.js
@@ -0,0 +1,21 @@
+var pais = 'EUA'
+
+var paisOrigem = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == paisOrigem){
+    console.log('você é nativo')
+} else {
+    console.log('você é extrangeiro')
+}
+
+
+pais = 'Brasil'
+if (pais != paisOrigem){
+    console.log('você é extrangeiro')
+} else {
+    console.log('você é nativo')
+}
+
+//As duas condições tem o mesmo efeito sendo uma questão de gosto optar por uma ou outra!
