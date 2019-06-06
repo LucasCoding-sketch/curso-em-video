eeab36b0e5219520c66c5162e75187c6acd1a4ad
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-06 05:51:09 -0300

    condicional simples

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/11-condidtion/exercicio08/script.js
@@ -0,0 +1,11 @@
+var velocidade = 50.5
+
+var limiteVelocidade = 60.0
+
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
+
+if (velocidade >= limiteVelocidade){
+    console.log('Você ultrapassou a velocidade permitida. Multado')
+}
+
+console.log('Dirija sempre usando o cinto de sgurança')
