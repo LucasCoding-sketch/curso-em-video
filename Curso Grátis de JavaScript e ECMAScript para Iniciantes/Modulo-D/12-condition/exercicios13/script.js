98e0fea20cf8254aa8838cadd99907d381799001
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-06 09:30:42 -0300

    exercicio 13, horas

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/12-condition/exercicios13/script.js
@@ -0,0 +1,13 @@
+var hora = 1
+
+console.log(`Agora são exatament ${hora} horas`)
+
+if(hora >= 6 && hora < 12){
+    console.log('bom dia')
+} else if(hora < 18 && hora >= 12){
+    console.log('Boa tarde')
+}else if(hora >= 18 && hora <=24){
+    console.log('Boa noite')
+} else {
+    console.log('Boa madrugada')
+}
