69d47a64df4ad69daa4a9c565d322c2496dc0c53
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-08 16:04:41 -0300

    init with array

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/19-variaveis-composta/sample.js
@@ -0,0 +1,11 @@
+let automoveis = [1,2,3]//array ou vetor e elementos; indice
+console.log(automoveis)
+automoveis[3] = 5
+console.log(automoveis)
+automoveis[5] = 5
+console.log(automoveis)
+automoveis.sort()
+console.log(automoveis)
+automoveis.push(-11)
+console.log(automoveis)
+console.log(automoveis[4])
