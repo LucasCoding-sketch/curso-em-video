cd2d99f3af80b75b3c175d614b84c4a02da9c7b5
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-08 06:06:12 -0300

    beging validate

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/14-ExerciciosJavaScript/exercicio16/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/14-ExerciciosJavaScript/exercicio16/script.js
@@ -10,3 +10,36 @@ function addDangerClass(htmlElement){
 }
 
 
+function verify(){
+    const ZERO = 0
+
+    var date  = new Date()
+
+    var year = date.getFullYear()
+
+    var informedYear = document.getElementById('textyear').value
+
+    var result = document.querySelector('div#result')
+
+    var radioGen = document.getElementsByName('radsex')
+
+    if (informedYear.length == ZERO || Number(informedYear) > year){
+        result.innerHTML = 'Verifique o ano e tente novamente.'
+        addDangerClass(result)
+    } else {        
+        var yearOld = year - Number(informedYear)
+
+        var genere = 'undefined genere'
+
+        if (radioGen[0].checked){
+            genere = radioGen[0].value
+        } else {
+            genere = radioGen[1].value
+        }
+
+        result.innerHTML = `${yearOld} + ${genere}`
+        addInfoClass(result)
+    }
+}
+
+document.querySelector("input#verify").addEventListener('click', verify)
