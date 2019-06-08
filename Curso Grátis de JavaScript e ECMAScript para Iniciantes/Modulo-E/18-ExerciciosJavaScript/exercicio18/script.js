f8dfae0068ad469d01e3f2017ee7ebddfec6c36a
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-08 15:30:30 -0300

    calcula a tabuada

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-E/18-ExerciciosJavaScript/exercicio18/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-E/18-ExerciciosJavaScript/exercicio18/script.js
@@ -1,1 +1,25 @@
-console.log('exercicio x')
+function calcularTabuada(event){
+    event.preventDefault()
+    console.log('calcularTabuada')
+    let numerator = document.getElementById('numerator').value
+    let resultDiv = document.querySelector('#resultError')
+    resultDiv.innerHTML == ""
+    document.querySelector('#result').innerHTML = ""
+
+    if(isNaN(numerator)|| numerator.length == 0){
+        
+        var div = document.createElement('div')
+
+        div.setAttribute('role', 'alert')
+        div.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show')
+        div.innerHTML = '<span><strong>informe um numero valido</strong></span><button class="close" data-dismiss="alert"><span aria-hidden="true">x</span></button>'
+        resultDiv.append(div)
+    }else{
+       for (let index = 1; index <= 10; index++) {
+           let options = document.createElement('option')
+           options.innerHTML = `${index} X ${numerator} = ${index * numerator}`
+           document.querySelector('#result').append(options)
+       } 
+    }
+}
+document.querySelector('form#calcularTabuada').addEventListener('submit',calcularTabuada)
