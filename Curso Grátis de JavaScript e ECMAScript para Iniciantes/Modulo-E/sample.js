b016b1339253f9f76ba3bc0c6a8c88f18d93c0fc
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-08 08:29:05 -0300

    loop com while

---
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-E/sample.js
@@ -0,0 +1,17 @@
+var x = 0
+var i = 0
+var result = ''
+
+while(x<8){
+    console.log(x)
+    x++
+}
+console.log('x', x)
+
+
+do {
+   i += 1
+   result += i + ' '
+   console.log(i)
+} while (i < 5);
+console.log('i',i)
