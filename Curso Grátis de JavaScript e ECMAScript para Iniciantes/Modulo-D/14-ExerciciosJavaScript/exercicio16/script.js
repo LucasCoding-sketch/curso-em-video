59c6b3e4e162d0527a461fa752b7431aa68caf43
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-08 06:17:24 -0300

    refac code

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/14-ExerciciosJavaScript/exercicio16/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-D/14-ExerciciosJavaScript/exercicio16/script.js
@@ -10,32 +10,38 @@ function addDangerClass(htmlElement){
 }
 
 
+function defineGenere(){
+
+    var radioGen = document.getElementsByName('radsex')
+
+    if (radioGen[0].checked){
+        return radioGen[0].value
+    } else {
+        return radioGen[1].value
+    }
+}
+
+
 function verify(){
     const ZERO = 0
 
-    var date  = new Date()
-
-    var year = date.getFullYear()
+    var year = new Date().getFullYear()
 
     var informedYear = document.getElementById('textyear').value
 
     var result = document.querySelector('div#result')
 
-    var radioGen = document.getElementsByName('radsex')
+    var isZeroInformadYearSize = informedYear.length == ZERO
 
-    if (informedYear.length == ZERO || Number(informedYear) > year){
+    var informedYearIsLessThanActualYear = Number(informedYear) > year
+
+    if (isZeroInformadYearSize || informedYearIsLessThanActualYear){
         result.innerHTML = 'Verifique o ano e tente novamente.'
         addDangerClass(result)
     } else {        
         var yearOld = year - Number(informedYear)
 
-        var genere = 'undefined genere'
-
-        if (radioGen[0].checked){
-            genere = radioGen[0].value
-        } else {
-            genere = radioGen[1].value
-        }
+        var genere = defineGenere()
 
         result.innerHTML = `${yearOld} + ${genere}`
         addInfoClass(result)
