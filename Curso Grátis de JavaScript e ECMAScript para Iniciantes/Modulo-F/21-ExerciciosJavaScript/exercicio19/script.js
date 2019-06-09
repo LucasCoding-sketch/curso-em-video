87926088b779590d0b9a257a9037a86e8cc67bbb
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-09 17:37:00 -0300

    add to array if element not exists, return array

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/21-ExerciciosJavaScript/exercicio19/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/21-ExerciciosJavaScript/exercicio19/script.js
@@ -1,5 +1,13 @@
+const ZERO = 0
 document.querySelector("form#show").addEventListener('submit',(event)=>{event.preventDefault()})
 
 let getNumber = ()=>{return window.document.querySelector('input#number').value}
 
 let hasSomeContent = (inputText)=>{return inputText.length>0}
+
+let addToArray = (currentArray, inputData)=>{
+    if(currentArray.indexOf(inputData) < 0){
+        currentArray.push(inputData)
+    }
+    return currentArray
+}
