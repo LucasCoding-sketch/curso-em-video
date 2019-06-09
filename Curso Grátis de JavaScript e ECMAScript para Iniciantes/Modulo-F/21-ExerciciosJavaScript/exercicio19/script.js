b87d83379b6bc03844006a6fbee4e1c968020cca
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-09 09:13:27 -0300

    verify is some content has content

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/21-ExerciciosJavaScript/exercicio19/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/21-ExerciciosJavaScript/exercicio19/script.js
@@ -1,3 +1,5 @@
 document.querySelector("form#show").addEventListener('submit',(event)=>{event.preventDefault()})
 
 let getNumber = ()=>{return window.document.querySelector('input#number').value}
+
+let hasSomeContent = (inputText)=>{return inputText.length>0}
