d633fe40bc3aa2557853ac7cc0c9908ada22ebaf
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-09 17:51:44 -0300

    update Select Element on html with new values

--- Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/21-ExerciciosJavaScript/exercicio19/script.js
+++ Curso Grátis de JavaScript e ECMAScript para Iniciantes/Modulo-F/21-ExerciciosJavaScript/exercicio19/script.js
@@ -1,13 +1,27 @@
 const ZERO = 0
-document.querySelector("form#show").addEventListener('submit',(event)=>{event.preventDefault()})
 
-let getNumber = ()=>{return window.document.querySelector('input#number').value}
+let informedNumbers = []
 
-let hasSomeContent = (inputText)=>{return inputText.length>0}
+document.querySelector("form#show").addEventListener('submit', (event) => { event.preventDefault() })
 
-let addToArray = (currentArray, inputData)=>{
-    if(currentArray.indexOf(inputData) < 0){
+let getNumber = () => { return window.document.querySelector('input#number').value }
+
+let hasSomeContent = (inputText) => { return inputText.length > 0 }
+
+let addToArray = (currentArray, inputData) => {
+    if (currentArray.indexOf(inputData) < ZERO) {
         currentArray.push(inputData)
     }
     return currentArray
+}
+
+
+let updateSelectElement = (currentArray) => {
+    let selectElement = window.document.querySelector('select#showResult')
+    $('#showResult').empty()
+    currentArray.forEach(e => {
+        let optionElement = window.document.createElement('option')
+        optionElement.innerHTML = e
+        selectElement.append(optionElement)
+    })
 }
